c0fa181e08638acbbf29be4d1c4e2579cc4eb099
Jake Babcock <jwbabcock@nic.edu>
2025-11-06 09:57:51 -0800

    more work done

--- results.h
+++ results.h
@@ -10,10 +10,13 @@ public:
     State handleInput(sf::Event& e, sf::RenderWindow& window);
     void update();
     void render(sf::RenderWindow& window);
+    void score(); //
 
+    void setScore(int score) { mScore.setString("Score: " + std::to_string(score)); }
 
 private:
     sf::Text mResults;
+    sf::Text mScore;
     Button mRules;
    // Button mPlayAgain;
     Button mLeave;
